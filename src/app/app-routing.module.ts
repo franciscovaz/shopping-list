b2cbd7452a24d2fa950d3d1c9d7fbaf301915a42
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-02 10:47:10 +0000

    config route params, passing dynamic parameters to links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
 import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
@@ -14,10 +15,13 @@ const appRoutes: Routes = [
   {
     path: 'recipes', component: RecipesComponent, children: [
       {
+        path: '', component: RecipeStartComponent
+      },
+      {
         path: 'list', component: RecipeListComponent
       },
       {
-        path: 'detail', component: RecipeDetailComponent
+        path: ':id', component: RecipeDetailComponent
       },
       {
         path: 'item', component: RecipeItemComponent
