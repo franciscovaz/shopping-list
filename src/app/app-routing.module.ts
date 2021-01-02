a46b957313e7ba0cd13993b6827938830d53ec57
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-02 11:01:55 +0000

    added editing routes and recipe edit component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
-import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
@@ -18,13 +17,14 @@ const appRoutes: Routes = [
         path: '', component: RecipeStartComponent
       },
       {
-        path: 'list', component: RecipeListComponent
+        path: 'new', component: RecipeEditComponent
       },
       {
         path: ':id', component: RecipeDetailComponent
       },
+
       {
-        path: 'item', component: RecipeItemComponent
+        path: ':id/edit', component: RecipeEditComponent
       }
     ]
   },
