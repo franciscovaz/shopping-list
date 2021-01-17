9efe5cd2f168e7d56963440389115e7e90af0a74
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-17 15:17:43 +0000

    updating existem items

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -38,4 +38,9 @@ export class ShoppingListService {
 
     this.ingredientsChanged.next(this.ingredients.slice());
   }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 }
