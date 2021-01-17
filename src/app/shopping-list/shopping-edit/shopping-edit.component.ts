6856036d7854588cef4a8ecce0ad6af3d897b4b3
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-17 15:23:25 +0000

    clear or cancel the form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -43,7 +43,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     }
     this.editMode = false;
     form.reset();
+  }
 
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
   }
 
   ngOnDestroy() {
