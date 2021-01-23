fd541d5f8d28b0255040aa1c83af4e1fa1c431d8
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-23 19:41:11 +0000

    reflecting the auth service in the UI Navigate to recipes when logged in

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,27 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   collapsed = true;
+  isAuthenticated = false;
+  private userSub: Subscription;
 
-  constructor(private dataStorage: DataStorageService) {}
+  constructor(private dataStorage: DataStorageService, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe(
+      userData => {
+        console.log('teste: ', userData);
+        // this.isAuthenticated = !userData ? false : true;
+        this.isAuthenticated = !!userData;
+      }
+    );
+  }
 
 
   onSaveData() {
@@ -16,5 +30,9 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataStorage.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
