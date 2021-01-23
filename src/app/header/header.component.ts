f50e71f914944472749386ca059c16a622db8143
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-23 15:12:20 +0000

    service to store recipes on firebase database

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,16 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
   collapsed = true;
+
+  constructor(private dataStorage: DataStorageService) {}
+
+
+  onSaveData() {
+    this.dataStorage.storeRecipes();
+  }
 }
