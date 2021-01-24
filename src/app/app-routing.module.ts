e6bc6c9bab356222833ecd099b9fda7cab66fa3f
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-24 12:06:06 +0000

    shopping list feature module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
-import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-
-  {
-    path: 'shopping-list', component: ShoppingListComponent, children: [
-      { path: 'edit', component: ShoppingEditComponent }
-    ]
-  },
   { path: 'auth', component: AuthComponent }
-
 ]
 
 @NgModule({
