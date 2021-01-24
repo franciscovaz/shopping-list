9df038977d8c43c31d9ce75280be3b28ea1a7095
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-24 10:03:25 +0000

    alert component displayed programmaticly with event and data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -57,6 +57,7 @@ import { PlaceholderDirective } from './shared/placeholder/placeholder.directive
       multi: true
     }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AlertComponent]
 })
 export class AppModule { }
