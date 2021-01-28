5952adfe29c126d902ce74945c8e8613ff9de43d
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-28 20:40:58 +0000

    updating and deleting ingredients

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -21,6 +21,27 @@ export function shoppingListReduce(state = initialState, action: ShoppingListAct
         ...state,
         ingredients: [...state.ingredients, ...action.payload]
       }
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[action.payload.index];
+      const updatedIngredient = {
+        ...ingredient,
+        ...action.payload.ingredient
+      }
+
+      const updatedIngredients = [...state.ingredients];
+      updatedIngredient[action.payload.index] = updatedIngredient;
+
+      return {
+        ...state,
+        ingredient: updatedIngredients
+      }
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: state.ingredients.filter((ingredient, index) => {
+          return index !== action.payload;
+        })
+      }
     default:
       return state;
   }
